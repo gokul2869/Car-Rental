b53886a0c41076c153ee1bb4ca85aefb1189f3f1
Gokul <161504895+gokul2869@users.noreply.github.com>
2024-09-26 01:22:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,16 @@ themeToggle.addEventListener('click', () => {
     localStorage.setItem('theme', 'dark');
   }
 });
+
+     
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbxto94iyUMq705xFVUBi2auXqqBuClDIAjL5mUGNAc/dev'
+  const form = document.forms['submit-to-google-sheet']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  })
+
+ 
